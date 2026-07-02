39cc1dccd2991710a9b446db07e6d3db983f8544
shree <shreekamdhenuchakrod@gmail.com>
2026-07-02 20:18:27 +0530

    added GitHub Pages Actions deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Kamdhenu-EMS/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
